5f40dff760b6fb034a4b913b603f39c5d5696f6d
jomnomz <jomeorenzdelacruz@gmail.com>
2026-02-14 00:38:07 +0800

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',   // 👈 ADD THIS LINE
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
